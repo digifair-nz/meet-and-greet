5d0762c1c433273308d276af7ea43414e4287446
Michael Shaimerden <sanzharshaimerden@gmail.com>
2020-05-09 13:09:37 +1200

    Prints out error message as a pop up

---
+++ digifair/src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+
+import Modal from "../../components/UI/Modal/Modal";
+import Aux from "../Auxiliary";
+
+// Explain code below
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null,
+    };
+
+    componentWillMount() {
+      this.reqInterceptor = axios.interceptors.request.use((req) => {
+        this.setState({ error: null });
+        console.log(req);
+        return req;
+      });
+      console.log("Check");
+      this.resInterceptor = axios.interceptors.response.use(
+        (res) => res,
+        (error) => {
+          console.log(error);
+          this.setState({ error: error });
+        }
+      );
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
+    }
+
+    errorConfirmedHandler = () => {
+      this.setState({ error: null });
+    };
+
+    render() {
+      console.log(this.state.error);
+      return (
+        <Aux>
+          <Modal
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
+          >
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      );
+    }
+  };
+};
+
+export default withErrorHandler;
