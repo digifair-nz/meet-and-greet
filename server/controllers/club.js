a0994f466496289f7776aac7dc5d48c6e2e7821c
PeterGoedeke <peterbgoedeke@gmail.com>
2020-05-09 22:57:58 +1200

    Added event creation route for clubs

--- server/controllers/club.js
+++ server/controllers/club.js
@@ -5,5 +5,24 @@ function getEvent() {
 
 }
 
+async function createEvent(req, res) {
+    if(!validate.isEvent(req.body, res)) return
+    
+    const event = new Event({
+        name: req.body.name,
+        description: req.body.description,
+        clubId: req.payload._id,
+        companiesAttending: req.body.companiesAttending
+    })
+    try {
+        await event.save()
+        return res.status(200).json(event)
+    }
+    catch(err) {
+        res.status(400).json(err)
+    }
+}
+
 module.exports = {
+    createEvent
 }
