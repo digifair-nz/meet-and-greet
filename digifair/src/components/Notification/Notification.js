591341bed6faf9733ac4ce4e2c0a9f3a8610f8ac
Michael Shaimerden <sanzharshaimerden@gmail.com>
2020-05-11 08:23:58 +1200

    Notification component helper function

---
+++ digifair/src/components/Notification/Notification.js
@@ -0,0 +1,19 @@
+import digifairLogo from "../../assets/company_logos/digifair_icon_notification.png";
+import notificationSound from "../../assets/audio/anxious.mp3";
+
+const sendNotification = (title, body) => {
+  // Notification
+
+  let notification = new Notification(title, {
+    body: body,
+    icon: digifairLogo,
+  });
+  const audio = new Audio(notificationSound);
+  audio.play();
+  notification.onclick = () => {
+    document.title = "Dashboard";
+    window.focus(); // Redirects to the window
+  };
+  document.title = "Digifair (1)";
+};
+export default sendNotification;
