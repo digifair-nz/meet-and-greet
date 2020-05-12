809548e6270b50c443d489fa1bd0ed531a5371e0
PeterGoedeke <peterbgoedeke@gmail.com>
2020-05-12 18:50:10 +1200

    Added enqueue, dequeue, and createQueue endpoints

--- server/controllers/queue.js
+++ server/controllers/queue.js
@@ -1,22 +1,50 @@
 const validate = require('./validation')
 
 const mongoose = require('mongoose')
-const Company = mongoose.model('Company')
+const Queue = mongoose.model('Queue')
 
-const get = require('./retrieval_helpers')
+const get = require('./database_interface')
 
 async function enqueue(req, res) {
-    const queues = await get.company(req, res, req.params._id, 'queues')
+    const result = await get.enqueueStatus.from(req, res)
 
-
+    if(!result.success) {
+        return res.status(500).json({ message: `Failed to enqueue` })
+    }
+    const positionInQueue = result.data.data.length - 1
+    return res.status(200).json({ position: positionInQueue })
 }
 
-function dequeue(req, res) {
-    
+async function dequeue(req, res) {
+    const result = await get.dequeueStatus.from(req, res)
+
+    if(!result.success) {
+        return res.status(500).json({ message: `Failed to dequeue` })
+    }
+    // notify on the websocket here
+    return res.status(200).send()
+}
+
+async function createQueue(req, res) {
+    if(!validate.isId({ _id: req.body.eventId}, res) || !validate.isId({ _id: req.body.companyId }, res)) {
+        return
+    }
+    const queue = new Queue({
+        eventId: req.body.eventId,
+        companyId: req.body.companyId
+    })
+    try {
+        await queue.save()
+        return res.status(200).send()
+    }
+    catch (error) {
+        return res.status(500).send()
+    }
 }
 
 
 module.exports = {
     enqueue,
-    dequeue
+    dequeue,
+    createQueue
 }
