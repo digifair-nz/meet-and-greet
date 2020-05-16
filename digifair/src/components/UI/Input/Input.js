12ca81ca790de317817dfa6bff26ce3c6425c785
Michael Shaimerden <sanzharshaimerden@gmail.com>
2020-05-16 21:33:12 +1200

    Auth container added
    Includes sign in form, button updates, icons and image added.

---
+++ digifair/src/components/UI/Input/Input.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+import classes from "./Input.module.css";
+
+import keyIcon from "../../../assets/icons/white-key.png";
+import userIcon from "../../../assets/icons/white-user.png";
+const input = (props) => {
+  const inputClasses = [classes.InputElement];
+
+  let inputElement = (
+    <input
+      className={inputClasses.join(" ")}
+      {...props.elementConfig}
+      value={props.value}
+      onChange={props.changed}
+    />
+  );
+
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inputClasses.push(classes.Invalid);
+  }
+
+  //   let validationError = null;
+  //   if (props.invalid && props.touched) {
+  //     validationError = (
+  //       <p style={{ color: "red" }}>Please enter a valid value!</p>
+  //     );
+  //   }
+  let icon = keyIcon;
+  let alt = "Key icon";
+
+  if (props.elementConfig.type === "email") {
+    icon = userIcon;
+    alt = "User icon";
+  }
+
+  return (
+    <div className={classes.Input}>
+      <div className={classes.IconWrapper}>
+        <img alt={alt} className={classes.InputIcon} src={icon} />
+      </div>
+
+      {inputElement}
+      {/* {validationError} */}
+    </div>
+  );
+};
+
+export default input;
---
+++ digifair/src/store/actions/studentAuth.js
@@ -0,0 +1,105 @@
+import axios from "../../axios-orders";
+
+import * as actionTypes from "./actionTypes";
+
+// NOTE: Possibly refractor if there is easy reusability between student,company and club authentication process
+export const studentAuthStart = () => {
+  return {
+    type: actionTypes.STUDENT_AUTH_START,
+  };
+};
+
+export const studentAuthSuccess = (token) => {
+  return {
+    type: actionTypes.STUDENT_AUTH_SUCCESS,
+    idToken: token,
+  };
+};
+
+export const studentAuthFail = (error) => {
+  return {
+    type: actionTypes.STUDENT_AUTH_FAIL,
+    error: error,
+  };
+};
+
+export const studentLogout = () => {
+  localStorage.removeItem("token");
+  // localStorage.removeItem("expirationDate");
+
+  return {
+    type: actionTypes.STUDENT_AUTH_LOGOUT,
+  };
+};
+
+export const studentCheckAuthTimeout = (expirationTime) => {
+  return (dispatch) => {
+    // setTimeout(() => {
+    //   // dispatch(studentLogout());
+    // }, expirationTime * 1000); // Expiration time should be based on the event expiration time
+  };
+};
+
+export const studentAuth = (email, password) => {
+  return (dispatch) => {
+    dispatch(studentAuthStart());
+
+    const authData = {
+      email: email,
+    };
+
+    axios
+      .post("/user/login/5ebdb9f8714a63289db621c8", authData)
+      .then((response) => {
+        // const expirationDate = new Date(
+        //   new Date().getTime() + response.data.expiresIn * 1000
+        // );
+        const token = response.headers["auth-token"];
+        localStorage.setItem("token", token);
+        // localStorage.setItem("expirationDate", expirationDate);
+
+        axios.defaults.headers.common["auth-token"] = token; // for all requests
+        dispatch(studentAuthSuccess(token));
+        // dispatch(studentCheckAuthTimeout(response.data.expiresIn));
+      })
+      .catch((err) => {
+        // console.log(err.headers);
+        // dispatch(studentAuthFail(err.response.data.error));
+      });
+  };
+};
+
+export const setStudentAuthRedirectPath = (path) => {
+  return {
+    type: actionTypes.SET_STUDENT_AUTH_REDIRECT_PATH,
+    path: path,
+  };
+};
+
+export const studentAuthCheckState = () => {
+  return (dispatch) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      dispatch(studentAuthSuccess(token));
+      // dispatch(
+      //   studentCheckAuthTimeout(
+      //     (expirationDate.getTime() - new Date().getTime()) / 1000
+      //   )
+      // );
+      // dispatch(studentLogout());
+    } //else {
+    // const expirationDate = new Date(localStorage.getItem("expirationDate"));
+    // if (expirationDate <= new Date()) {
+    //   // dispatch(studentLogout());
+    // } else {
+    //   const userId = localStorage.getItem("userId");
+    //   dispatch(studentAuthSuccess(token, userId));
+    //   dispatch(
+    //     studentCheckAuthTimeout(
+    //       (expirationDate.getTime() - new Date().getTime()) / 1000
+    //     )
+    //   );
+    // }
+    //}
+  };
+};
